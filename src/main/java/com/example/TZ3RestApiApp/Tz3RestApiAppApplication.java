f5bee0f891c112dcc1d6604ac81d0c5c393646dd
MegaDog666 <Moonandre9999@mail.ru>
2025-01-16 11:35:28 +0300

    Begin

---
+++ src/main/java/com/example/TZ3RestApiApp/Tz3RestApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.TZ3RestApiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tz3RestApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tz3RestApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TZ3RestApiApp/Tz3RestApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TZ3RestApiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tz3RestApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
